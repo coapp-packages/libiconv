b484b149545e099a4f6a10b9fd1179192b3a08b6
Tim Rogers <tiroger@microsoft.com>
2013-04-26 09:47:24 -0700

    changes to meet current standards

---
+++ COPKG/all.bat
@@ -0,0 +1,49 @@
+@echo off
+setlocal
+
+if "%1"=="clean" goto :clean
+if "%1"=="noclean" (
+	set __NOCLEAN__=true
+	shift)
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" amd64
+call :build x64 Release v110
+call :build x64 Debug v110
+endlocal
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+call :build Win32 Release v110
+call :build Win32 Debug v110
+endlocal
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
+call :build x64 Release v100
+call :build x64 Debug v100
+endlocal
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+call :build Win32 Release v100
+call :build Win32 Debug v100
+endlocal
+
+if "%__NOCLEAN__%"=="true" goto :eof
+
+goto :clean
+
+goto :eof
+
+:build
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=DynamicLibrary  .\libiconv\libiconv.vcxproj
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=StaticLibrary  .\libiconv\libiconv.vcxproj
+
+goto :eof
+
+:clean
+REM rd /s /q v90
+rd /s /q v100
+rd /s /q v110
+
